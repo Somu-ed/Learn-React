c439afa1723d678a2bb632657f75c7f883d1c205
Somu-ed <satpathysoumyajeet.99@gmail.com>
2020-09-14 12:29:30 +0530

    Forms 2.0

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,28 +3,28 @@ import React  from 'react';
 const Ninjas = ({ninjas}) => {
     // console.log(this.props);
     // const {ninjas} = props; //not needed as it's destructured directly in func parameters
-    // const ninjaList = ninjas.map(ninja => {
-    //     if(ninja.age > 20){
-    //         return(
-    //         <div className="ninja" key={ninja.id}>
-    //             <div>Name: {ninja.name} </div>
-    //             <div>Age: {ninja.age} </div>
-    //             <div>Belt: {ninja.belt} </div>
-    //         </div>
-    //         )
-    //     } else{
-    //         return null
-    //     }
-    // })
     const ninjaList = ninjas.map(ninja => {
-        return ninja.age > 20 ? (
+        if(ninja.age > 20){
+            return(
             <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name} </div>
                 <div>Age: {ninja.age} </div>
                 <div>Belt: {ninja.belt} </div>
-             </div>
-        ) : null;
+            </div>
+            )
+        } else{
+            return null
+        }
     })
+    // const ninjaList = ninjas.map(ninja => {
+    //     return ninja.age > 20 ? (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>Name: {ninja.name} </div>
+    //             <div>Age: {ninja.age} </div>
+    //             <div>Belt: {ninja.belt} </div>
+    //          </div>
+    //     ) : null;
+    // })
     return(
         <div className="ninja-list">
             { ninjaList }
