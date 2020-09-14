deac68462cf04b800c953d0c611e3217c18bab4e
Somu-ed <satpathysoumyajeet.99@gmail.com>
2020-09-14 12:03:50 +0530

    stateless components (functions)

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,24 +1,22 @@
-import React, {Component}  from 'react';
+import React  from 'react';
 
-class Ninjas extends Component {
-    render(){
-        // console.log(this.props);
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return(
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name} </div>
-                    <div>Age: {ninja.age} </div>
-                    <div>Belt: {ninja.belt} </div>
-                </div>
-            )
-        })
+const Ninjas = ({ninjas}) => {
+    // console.log(this.props);
+    // const {ninjas} = props; //not needed as it's destructured directly in func parameters
+    const ninjaList = ninjas.map(ninja => {
         return(
-            <div className="ninja-list">
-                { ninjaList }
+            <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name} </div>
+                <div>Age: {ninja.age} </div>
+                <div>Belt: {ninja.belt} </div>
             </div>
         )
-    }
+    })
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+    )
 }
 
 export default Ninjas
